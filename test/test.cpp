b258a6956dc62e99bbafdef0ea699f97a204b585
LiZhenhuan1019 <lizhenhuan1019@qq.com>
2016-09-22 20:36:19 +0800

    rename Graph.h to graph.h

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cassert>
 #define private public
-#include "Graph/include/Graph.h"
+#include "Graph/include/graph.h"
 using namespace std;
 using namespace lzhlib;
 using namespace lzhlib::detail;
